92ccd1bddcd5f0839b531268e985073d2aeebf2f
jip1984 <58789786+jip1984@users.noreply.github.com>
2021-01-22 17:07:00 +0000

    started on bike project
    Done the  change color part

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -1,6 +1,28 @@
-const blueBtn = documnet.getElementById('blueBtn');
+const blueBtn = document.getElementById("blueBtn");
+blueBtn.addEventListener("click", function() {
+    document.getElementsByClassName("jumbotron")[0].style.backgroundColor = "#588fbd";
+    document.getElementsByClassName("btn btn-primary btn-lrg")[0].style.backgroundColor = "#ffa500";
+    document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.backgroundColor = "black";
+    document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.color = "white";
+});
 
-blueBtn.addEventListener('click' function(){
+const orangeBtn = document.getElementById('orangeBtn');
 
-    
-})
+const orangeButton = document.getElementById("orangeBtn");
+orangeButton.addEventListener("click", function() {
+    document.getElementsByClassName("jumbotron")[0].style.backgroundColor = "#f0ad4e";
+    document.getElementsByClassName("btn btn-primary btn-lrg")[0].style.backgroundColor = "#5751fd";
+    document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.backgroundColor = "#31b0d5";
+    document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.color = "white";
+});
+
+const greenButton = document.getElementById("greenBtn");
+greenButton.addEventListener("click", function() {
+    document.getElementsByClassName("jumbotron")[0].style.backgroundColor = "#87ca8a";
+    document.getElementsByClassName("btn btn-primary btn-lrg")[0].style.backgroundColor = "black";
+    document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.backgroundColor = "#8c9c08";
+    document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.color = "black";
+});
+
+const submitBtn = document.getElementById('submit');
+submitBtn.addEventListener
