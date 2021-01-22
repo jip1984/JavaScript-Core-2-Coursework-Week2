e8455dee04880c816e31a5fc06d2c69f0a60fa2a
jip1984 <58789786+jip1984@users.noreply.github.com>
2021-01-22 18:00:24 +0000

    finished

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -24,4 +24,29 @@ greenButton.addEventListener("click", function() {
     document.getElementsByClassName("btn btn-secondary btn-lrg")[0].style.color = "black";
 });
 
-const submitBtn = document.getElementById('submit');
+
+// form button
+let submitButton = document.querySelector("form button");
+
+function formButton(event) {
+    event.preventDefault();
+    let emailField = document.querySelector("#exampleInputEmail1");
+    let nameField = document.querySelector("#example-text-input");
+    let describeField = document.querySelector("#exampleTextarea");
+    console.log(nameField.value.length);
+  
+      if (!emailField.value.length > 0 || !emailField.value.includes("@")){
+      emailField.style.backgroundColor = "#ff0000";
+      } else if (!nameField.value.length > 0) {
+      nameField.style.backgroundColor = "#ff0000";
+      } else if (!describeField.value.length > 0) {
+       describeField.style.backgroundColor = "#ff0000";
+    } else {
+          alert("Thank you for filling out the form");
+          nameField.value = "";
+          emailField.value = "";
+          describeField.value = "";
+    }
+  }
+  
+  submitButton.addEventListener("click", (event) => formButton(event));
